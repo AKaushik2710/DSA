04894d65a742aa4cc03bd5a695526cd62a80f7dd
AKaushik2710 <amankaushik2710ak@gmail.com>
2025-05-11 13:56:35 +0530

    Template Added

---
+++ arrays/template.js
@@ -0,0 +1,28 @@
+function template(func,n,ques,ques2){
+	const readline = require('readline');
+	const rl = readline.createInterface({
+		input : process.stdin,
+		output : process.stdout
+	});
+	
+	if(n===2){
+		rl.question(ques, (answer)=>{
+			const input = answer.split(',').map(Number);
+			rl.question(ques2, (answer)=>{
+				const input2 = Number(answer);
+				console.log(func(input,input2));
+				rl.close();
+			})
+		})
+	}
+	else{
+		rl.question(ques, (answer)=>{
+			const input = answer.split(',').map(Number);
+			console.log(func(input), input);
+			rl.close();
+		})
+	}
+}
+
+
+module.exports = template;
